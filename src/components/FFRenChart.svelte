780295630d83bbe31ed62e75e1cbfab5bbaec75e
Joseph Ricafort <josephricafortjr@gmail.com>
2023-01-08 01:16:03 +0800

    Encode indicator variables to full texts

--- src/components/FFRenChart.svelte
+++ src/components/FFRenChart.svelte
@@ -112,13 +112,29 @@
         const valFixed = sIndex >= 0 ? sData[sIndex].data[ind].toFixed(0) : 0
         const valRound = Math.round(valFixed / 100) * 100
         const perc = sIndex >= 0 ? ((valFixed / sData[sIndex].data.total) * 100).toPrecision(2) : 0
-        return `${ind}: ${valRound} (${perc}%)`
+        const indClean = !togglePercap ? ind.replace("prod", "") : 
+            ind.includes("prodpercap") ? ind.replace("prodpercap", "") : ind.replace("percap", "")
+        return `${indClean}: ${valRound} (${perc}%)`
     }
 
     // To prevent throwing an error where mouseover and mouseout requires
     // accompanying onfocus and onblur
     function handleFocus() {}
     function handleBlur() {}
+
+    // Encode legend labels
+    const TWH = "Production (TWh)"
+    const PC_KWH = "Production per capita (kWh)"
+    $: energyUnit = !togglePercap ? TWH : PC_KWH
+    $: encodeInd = (ind) => {
+        switch(ind) {
+            case indicatorsUsed[0]: return `Coal ${energyUnit}`;
+            case indicatorsUsed[1]: return `Gas ${energyUnit}`;
+            case indicatorsUsed[2]: return `Oil ${energyUnit}`;
+            case indicatorsUsed[3]: return `Renewable ${energyUnit}`;
+            default: return "";
+        }
+    }
 
 </script>
 
@@ -131,7 +147,7 @@
         {#each indicatorsUsed as ind, i}
             <div class="legend-container">
                 <div class="legend-color" style={`background-color: ${schemeCategory10[i]};`}></div>
-                <span class="">{ind}</span>
+                <span class="">{encodeInd(ind)}</span>
             </div>
         {/each}
     </div>
@@ -154,52 +170,6 @@
                     class="path"
                 />
             {/each}
-        </g>
-        <g class="tooltip">
-            <text
-                class="tt-year"
-                x={isCursorOnRight ? mouseX - 10 : mouseX + 10}
-                y="15"
-                text-anchor={isCursorOnRight ? "end" : "start"}
-                visibility={ttVisibility}
-            >
-                {year}
-            </text>
-            {#each seriesData as sData, i}
-                <text
-                    class="tt-indicator"
-                    x={isCursorOnRight ? mouseX - 10 : mouseX + 10}
-                    y="40"
-                    text-anchor={isCursorOnRight ? "end" : "start"}
-                    dy={(i + 1) * 20}
-                    visibility={ttVisibility}
-                >
-                    {getTooltipText(sData, i)}
-                </text>
-            {/each}
-            <g class="vline">
-                <line 
-                    x1={mouseX - 2.5}
-                    x2={mouseY - 2.5}
-                    y1="0"
-                    y2={yMax}
-                    stroke="#000000"
-                    opacity="0.5"
-                    visibility={ttVisibility}
-                />
-            </g>
-            <g class="points-group">
-                {#each seriesData as sdpt}
-                    <circle
-                        r="5"
-                        cx={pointX(sdpt)}
-                        cy={pointY(sdpt)}
-                        stroke="#000000"
-                        fill="none"
-                        visibility={ttVisibility}
-                    />
-                {/each}
-            </g>
         </g>
         <g class="axes">
             <g class="x-axis">
@@ -249,19 +219,81 @@
                 {/each}
             </g>
         </g>
+        <g class="tooltip">
+            <g class="tt-values">
+                <rect
+                    x={isCursorOnRight ? mouseX - 185 : mouseX}
+                    y="35"
+                    width="180"
+                    height="100"
+                    fill="#fff"
+                    stroke="#aaa"
+                    stroke-width="0.5"
+                    fill-opacity="0.75"
+                    visibility={ttVisibility}
+                />
+                <text
+                    class="tt-year"
+                    x={isCursorOnRight ? mouseX - 10 : mouseX + 10}
+                    y="25"
+                    text-anchor={isCursorOnRight ? "end" : "start"}
+                    visibility={ttVisibility}
+                >
+                    {year}
+                </text>
+                {#each seriesData as sData, i}
+                    <text
+                        class="tt-indicator"
+                        x={isCursorOnRight ? mouseX - 10 : mouseX + 10}
+                        y="40"
+                        text-anchor={isCursorOnRight ? "end" : "start"}
+                        dy={(i + 1) * 20}
+                        visibility={ttVisibility}
+                        fill={schemeCategory10[i]}
+                    >
+                        {getTooltipText(sData, i)}
+                    </text>
+                {/each}
+            </g>
+            <g class="vline">
+                <line 
+                    x1={mouseX - 2.5}
+                    x2={mouseY - 2.5}
+                    y1="0"
+                    y2={yMax}
+                    stroke="#000000"
+                    opacity="0.5"
+                    stroke-dasharray="2 3"
+                    visibility={ttVisibility}
+                />
+            </g>
+            <g class="points-group">
+                {#each seriesData as sdpt}
+                    <circle
+                        r="5"
+                        cx={pointX(sdpt)}
+                        cy={pointY(sdpt)}
+                        stroke="#000000"
+                        fill="none"
+                        visibility={ttVisibility}
+                    />
+                {/each}
+            </g>
+        </g>
     </svg>
 </div>
 
 <style>
 .area-chart {
     display: block;
-    height: 450px;
+    height: 500px;
     width: 100%;
 	margin: 0 0 2em 0;
+    font-size: 14px;
 }
 
 .ui-controls {
-    padding: 5px;
+    padding: 10px;
     border: 1px solid #ccc;
     margin: 5px 0;
     text-align: left;
@@ -270,7 +302,7 @@
 .legends {
     display: flex;
     flex-flow: row;
-    align-items: center;
+    /* align-items: center; */
     padding: 5px;
     border: 1px solid #ccc;
     margin: 5px 0;
@@ -278,11 +310,13 @@
 
 .legend-container {
     padding: 5px 10px;
+    text-align: left;
 }
 
 .legend-color {
     height: 10px;
     width: 10px;
     display: inline-block;
+    margin-right: 5px;
 }
 </style>
